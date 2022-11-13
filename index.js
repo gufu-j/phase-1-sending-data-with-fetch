3a3aeb25891b7dc1a6c8ac79914433202f811729
diego <guatemaladiegojose@gmail.com>
2022-11-13 14:55:24 -0500

    commit tree 21d585b179b0894ac39c2772f1aaf8869b814348

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Add your code here
+
+function submitData(name, email) {
+    
+    const configurationObject = {
+        method: "POST",
+
+        headers: {
+           
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+
+        },
+        body: JSON.stringify({
+            name: "Steve",
+            email: "steve@steve.com"
+        }),
+
+    };
+    
+    return fetch ("http://localhost:3000/users", configurationObject)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (object){
+        console.log(object);
+        console.log(object.id);
+        document.body.innerHTML= object.id
+    })
+    .catch(function(error){
+        
+        
+        document.body.innerHTML = error.message;
+
+
+        
+    });
+
+}
+
+
+
